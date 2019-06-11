2cb1778639994197730eee153981716a71348c4f
Shubham Banjare <shubhamthegreat29@gmail.com>
2019-06-11 13:26:17 +0530

    1st commi

---
+++ test1.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv2.imread('D:/Shubham/Python Program/Open CV Projects/1.jpg',cv2.IMREAD_GRAYSCALE)
+
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllwindows()
+#hello
